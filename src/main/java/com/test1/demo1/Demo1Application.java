b44973e8510177985f7712a94164d41734a9c51a
LaQicka <laqicka1@outlook.com>
2022-03-11 17:10:55 +0300

    Starting project

---
+++ src/main/java/com/test1/demo1/Demo1Application.java
@@ -0,0 +1,15 @@
+package com.test1.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.test1.demo1.controllers"})
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
