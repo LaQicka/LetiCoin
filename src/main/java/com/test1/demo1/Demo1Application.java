101346782950b47239616e43aff60c7aac69f94b
LaQicka <laqicka1@outlook.com>
2022-03-11 17:14:30 +0300

    Testing nik branch

--- src/main/java/com/test1/demo1/Demo1Application.java
+++ src/main/java/com/test1/demo1/Demo1Application.java
@@ -3,7 +3,7 @@ package com.test1.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+// testing nik branch
 @SpringBootApplication
 @ComponentScan({"com.test1.demo1.controllers"})
 public class Demo1Application {
