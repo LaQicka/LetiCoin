ffe999c63e29e6ecea8f0db62c4f96311230c567
LaQicka <laqicka1@outlook.com>
2022-05-26 05:41:26 +0300

    create CourseDAO 001

---
+++ src/main/java/com/leticoin/webapp/model/Course.java
@@ -0,0 +1,41 @@
+package com.leticoin.webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long course_id;
+
+    private String name;
+
+    public Course(){
+        this.course_id = Long.valueOf(0);
+        this.name = "course is not available";
+    }
+
+    public Course(Long id, String name){
+        this.course_id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return course_id;
+    }
+
+    public void setId(Long id) {
+        this.course_id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
