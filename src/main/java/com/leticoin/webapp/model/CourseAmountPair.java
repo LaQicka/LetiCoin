e29784e27a8d945c3d8538b09d29e4c80b2b7f40
LaQicka <laqicka1@outlook.com>
2022-06-04 18:28:46 +0300

    add Extra class - CourseAmountPair

---
+++ src/main/java/com/leticoin/webapp/model/CourseAmountPair.java
@@ -0,0 +1,27 @@
+package com.leticoin.webapp.model;
+
+public class CourseAmountPair {
+    Course course;
+    Integer amount;
+
+    public CourseAmountPair(Course course, Integer amount) {
+        this.course = course;
+        this.amount = amount;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
