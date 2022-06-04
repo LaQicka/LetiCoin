8066e83c41cf412ec44cc9513b69f45b71436dd3
LaQicka <laqicka1@outlook.com>
2022-06-04 18:27:22 +0300

    update User.java

--- src/main/java/com/leticoin/webapp/model/User.java
+++ src/main/java/com/leticoin/webapp/model/User.java
@@ -3,19 +3,14 @@ package com.leticoin.webapp.model;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long user_id;
 
     private String name, surname;
-
-    @ManyToMany()
-    private List<Long> courses = new ArrayList<>();
 
     public User() {
         name = "Jim";
@@ -51,15 +46,4 @@ public class User {
         this.surname = surname;
     }
 
-    public List<Long> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Long> courses) {
-        this.courses = courses;
-    }
-
-    public void addCourse(Long course_id){
-        courses.add(course_id);
-    }
 }
