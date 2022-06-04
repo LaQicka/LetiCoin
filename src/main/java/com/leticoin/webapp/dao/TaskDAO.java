da3c940201d66955685c6b5924a6794a239f7931
LaQicka <laqicka1@outlook.com>
2022-06-04 18:27:02 +0300

    update TaskDAO.java - add Database logic

--- src/main/java/com/leticoin/webapp/dao/TaskDAO.java
+++ src/main/java/com/leticoin/webapp/dao/TaskDAO.java
@@ -36,7 +36,12 @@ public class TaskDAO {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Task WHERE taskid = " + id);
             while(resultSet.next()){
-                Task task = new Task(resultSet.getLong("taskid"),resultSet.getString("task"),resultSet.getString("condition"),resultSet.getString("answer"),resultSet.getLong("courseid"));
+                Task task = new Task(resultSet.getLong("taskid"),
+                                    resultSet.getString("task"),
+                                    resultSet.getString("condition"),
+                                    resultSet.getString("answer"),
+                                    resultSet.getLong("courseid"),
+                                    resultSet.getInt("price"));
                 return task;
             }
         } catch (SQLException e) {
@@ -51,12 +56,50 @@ public class TaskDAO {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Task WHERE courseid = " + id);
             while(resultSet.next()){
-                Task task = new Task(resultSet.getLong("taskid"),resultSet.getString("task"),resultSet.getString("condition"),resultSet.getString("answer"),resultSet.getLong("courseid"));
+                Task task = new Task(resultSet.getLong("taskid"),
+                                    resultSet.getString("task"),
+                                    resultSet.getString("condition"),
+                                    resultSet.getString("answer"),
+                                    resultSet.getLong("courseid"),
+                                    resultSet.getInt("price"));
                 courseTasks.add(task);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return courseTasks;
+    }
+
+    public boolean isSolved(long user_id, long task_id){
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "select count(*) <> 0 as answer from solved where userid = "+ user_id +" and taskid = " + task_id);
+            resultSet.next();
+            return resultSet.getBoolean("answer");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void solve(long user_id, long task_id, long course_id){
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "select * from personcourses where userid = "+user_id+" and courseid = "+course_id);
+            resultSet.next();
+            int amount = resultSet.getInt("amount");
+            resultSet = statement.executeQuery("select * from task where taskid = " + task_id);
+            resultSet.next();
+            amount += resultSet.getInt("price");
+            statement.executeUpdate(
+                    "insert into solved values(" + user_id + "," + task_id + " )");
+            statement.executeUpdate(
+                    "update personcourses set amount = " + amount + " where userid = " + user_id + " and courseid = " + course_id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return;
     }
 }
