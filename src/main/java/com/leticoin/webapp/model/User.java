c9e73185890a93df221e4792f91dbf67f17438ce
LaQicka <laqicka1@outlook.com>
2022-06-05 23:47:54 +0300

    update User database logic

--- src/main/java/com/leticoin/webapp/model/User.java
+++ src/main/java/com/leticoin/webapp/model/User.java
@@ -1,8 +1,7 @@
 package com.leticoin.webapp.model;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 @Entity
 public class User {
@@ -10,16 +9,28 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long user_id;
 
-    private String name, surname;
+    private String name, surname, email, password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+
+    private Collection<Role> roles;
 
     public User() {
         name = "Jim";
         surname = "Jones";
     }
 
-    public User(Long id, String name) {
+    public User(Long id, String name, String surname, String email, String password, Collection<Role> roles) {
         this.user_id = id;
         this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
     }
 
     public Long getId() {
@@ -46,4 +57,27 @@ public class User {
         this.surname = surname;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
 }
