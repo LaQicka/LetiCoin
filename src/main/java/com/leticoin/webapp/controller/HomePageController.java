c148bd42e4c95812a1cd00b1f48d377b6fa4983b
imserega <sergei1420011@gmail.com>
2022-06-06 00:06:46 +0300

    Add Controllers

--- src/main/java/com/leticoin/webapp/controller/HomePageController.java
+++ src/main/java/com/leticoin/webapp/controller/HomePageController.java
@@ -4,10 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomePageController{
+public class HomePageController {
 
-        @GetMapping("/")
-        public String home(){
-            return "/home";
-        }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/courses")
+    public String courses() {
+        return "courses";
+    }
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "shop";
+    }
+
+    @GetMapping("/faq")
+    public String faq() {
+        return "faq";
+    }
+
 }
